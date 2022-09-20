02ad54e585063c9b172d135d0d98ae2cd8337aa6
masonSmigel <mgsmigel@gmail.com>
2022-09-20 18:18:29 -0400

    added auto fill feild to publish script.

--- scripts/sas_pipe/maya/ui/publishFile.py
+++ scripts/sas_pipe/maya/ui/publishFile.py
@@ -4,7 +4,11 @@ import inspect
 import re
 import logging
 
+from functools import partial
+
 import maya.OpenMayaUI as omui
+import maya.cmds as cmds
+
 from PySide2 import QtCore
 from PySide2 import QtGui
 from PySide2 import QtUiTools
@@ -21,6 +25,8 @@ import sas_pipe.maya.file as sas_file
 MAYA_FILTER = "Maya Files (*.ma *.mb);;Maya ASCII (*.ma);;Maya Binary (*.mb)"
 
 logger = logging.getLogger(__name__)
+
+LOCAL_TO_PUBLISH = '../publish'
 
 
 def maya_main_window():
@@ -51,7 +57,7 @@ class FunctionExecuter(QtWidgets.QWidget):
 
     def create_layouts(self):
         main_layout = QtWidgets.QVBoxLayout(self)
-        main_layout.setContentsMargins(0,0,0,0)
+        main_layout.setContentsMargins(0, 0, 0, 0)
         main_layout.addWidget(self.tree_wid)
 
     def create_connections(self):
@@ -132,7 +138,7 @@ class PublishEntityUi(QtWidgets.QDialog):
         super(PublishEntityUi, self).__init__(parent)
 
         self.setWindowTitle("Publish Entity")  # Window title name
-        self.setBaseSize(405, 460)  # Window minimum width
+        self.setBaseSize(500, 560)  # Window minimum width
 
         self.setWindowFlags(QtCore.Qt.Window)  # window type
 
@@ -141,9 +147,13 @@ class PublishEntityUi(QtWidgets.QDialog):
         # make Maya remember the window position
         self.setProperty("saveWindowPref", True)
 
+        self.scriptJobID = -1
+
         self.create_widgets()
         self.create_layout()
         self.create_connections()
+
+        self.fillFeilds()
 
     def create_widgets(self):
         self.tree = FunctionExecuter()
@@ -152,10 +162,11 @@ class PublishEntityUi(QtWidgets.QDialog):
         self.discription_te = QtWidgets.QTextEdit()
         self.discription_te.setFixedHeight(100)
 
-        self.out_file_path_selector = pathSelector.PathSelector("Output File", cap="Select an output file", ff=MAYA_FILTER,
-                                                                fm=0)
-        self.save_version_cb = QtWidgets.QCheckBox("Save Version")
-        self.save_version_cb.setChecked(True)
+        self.out_file_path_selector = pathSelector.PathSelector(
+            "Output File",
+            caption="Select an output file",
+            fileFilter=MAYA_FILTER,
+            fileMode=0)
 
         self.cancel_btn = QtWidgets.QPushButton("Cancel")
         self.cleanup_btn = QtWidgets.QPushButton("Run Cleanup")
@@ -173,7 +184,6 @@ class PublishEntityUi(QtWidgets.QDialog):
 
         path_layout = QtWidgets.QHBoxLayout()
         path_layout.addWidget(self.out_file_path_selector)
-        path_layout.addWidget(self.save_version_cb)
 
         main_layout.addWidget(QtWidgets.QLabel("Publish Steps:"))
         main_layout.addWidget(self.tree)
@@ -188,36 +198,86 @@ class PublishEntityUi(QtWidgets.QDialog):
         self.cleanup_btn.clicked.connect(self.publish)
         self.publish_btn.clicked.connect(self.save_and_publish)
 
+    def fillFeilds(self):
+        """
+        Auto fill the required feilds
+        """
+        currentFile = cmds.file(q=True, sn=True)
+
+        if not currentFile:
+            logger.warning("No file is currently open. Please save a file before attempting to publish")
+            return
+
+        basePath = os.path.dirname(currentFile)
+        fileName = os.path.basename(currentFile)
+
+        # construct a new filepath
+        if not len(fileName.split("_")) == 4:
+            logger.error("File is not a valid work file. Please open a SAS piped work file or check your naming convention.")
+            return
+
+        base = fileName.split("_v")[0]
+        extension = fileName.split(".")[-1]
+
+        guessFileName = "{}.{}".format(base, extension)
+        guessPathName = os.path.abspath(os.path.join(basePath, LOCAL_TO_PUBLISH))
+        guessFullPathName = os.path.join(guessPathName, guessFileName)
+
+        # set the path selector locally to the current file and set the path.
+        self.out_file_path_selector.setRelativePath(basePath)
+        self.out_file_path_selector.selectPath(guessFullPathName)
+
     def publish(self):
         self.tree.execute_all()
 
     def save_and_publish(self):
         self.publish()
 
-        path = self.out_file_path_selector.get_path()
+        path = self.out_file_path_selector.getPath()
         if path:
-            sas_file.saveAs(self.out_file_path_selector.get_path())
-            if self.save_version_cb.isChecked():
+            sas_file.saveAs(self.out_file_path_selector.getPath())
 
-                dir_name = os.path.dirname(path)
-                file_name = os.path.basename(path)
+            # save a versioned file
+            dir_name = os.path.dirname(path)
+            file_name = os.path.basename(path)
 
-                # get the version directory, file
-                version_dir = os.path.join(dir_name, 'versions')
-                filebase = ".".join(file_name.split('.')[:-1])
-                fileext = file_name.split('.')[-1]
+            # get the version directory, file
+            version_dir = os.path.join(dir_name, 'versions')
+            filebase = ".".join(file_name.split('.')[:-1])
+            fileext = file_name.split('.')[-1]
 
-                # format the new file name and file path
-                version_file = "{}_{}.{}".format(filebase, 'v000', fileext)
-                version_path = os.path.join(version_dir, version_file)
+            # format the new file name and file path
+            version_file = "{}_{}.{}".format(filebase, 'v000', fileext)
+            version_path = os.path.join(version_dir, version_file)
 
-                # make the output directory and save the file
-                sas_path.make_dir(version_dir)
-                version_path = sas_file.incrimentSave(version_path, log=False)
-                logger.info("out rig archived: {}".format(version_path))
+            # make the output directory and save the file
+            sas_path.make_dir(version_dir)
+            version_path = sas_file.incrimentSave(version_path, log=False)
+            logger.info("out file archived: {}".format(version_path))
 
     def update_description(self):
         self.discription_te.setText(self.tree.get_item_docstring())
+
+    def showEvent(self, e):
+        """ override the show event to add the script job. """
+        super(PublishEntityUi, self).showEvent(e)
+        self.setScriptJobEnabled(True)
+
+    def closeEvent(self, *args, **kwargs):
+        super(PublishEntityUi, self).closeEvent(*args)
+        self.setScriptJobEnabled(False)
+
+    def setScriptJobEnabled(self, enabled):
+        """
+        Set the state of the script job.
+        The script job ensures the widget displays changes when the scene changes
+        """
+        if enabled and self.scriptJobID < 0:
+            self.scriptJobID = cmds.scriptJob(event=["SceneOpened", partial(self.fillFeilds)],
+                                              protected=True)
+        elif not enabled and self.scriptJobID > 0:
+            cmds.scriptJob(kill=self.scriptJobID, f=True)
+            self.scriptJobID = -1
 
 
 if __name__ == '__main__':
