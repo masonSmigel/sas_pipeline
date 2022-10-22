ce15d432be6d82174769e97974564451ead5487e
masonSmigel <mgsmigel@gmail.com>
2022-10-21 22:22:53 -0400

    added quick camera export option to the layout menu

---
+++ scripts/sas_pipe/maya/depts/layout/menu.py
@@ -0,0 +1,70 @@
+"""
+This module creates a menu for a SAS pipeline
+"""
+import imp
+import sas_pipe.common as common
+import sas_pipe.constants
+from sas_pipe.maya.widgets import subMenuBase
+
+
+class LayoutMenu(subMenuBase.SubMenu):
+    def __init__(self, name='Modelling', parent=None, iconsPath=sas_pipe.constants.ICONS_PATH):
+        super(LayoutMenu, self).__init__(name=name, parent=parent, iconPath=iconsPath)
+
+    def build(self):
+        """Build the layout subMenu"""
+
+        self.addMenuItem("Quick Camera Export", command=cleanSelectedModel)
+
+
+# functions explicitly connected to rigamajig2
+def exportCameras(*args):
+    """ Export cameras"""
+
+    cameras = [c for c in cmds.ls("seq*") if cmds.nodeType(c) == "camera"]
+
+    if not len(cameras) > 0:
+        raise Exception("Please enusre you have at LEAST one camera following the naming convention: 'seq010_0010'. ")
+
+    minTime = cmds.playbackOptions(q=True, min=True)
+    maxTime = cmds.playbackOptions(q=True, max=True)
+
+    # set the camera to the paybackMin
+    cmds.currentTime(minTime, u=True)
+
+    for cam in cameras:
+        camTrs = cmds.listRelatives(cam, p=True)[0]
+
+        # first create a new camera
+        camToExport = cmds.duplicate(cam)
+        cmds.parent(camToExport, w=True)
+
+        # Constrain the source camera to the new camera
+        camConstraint = cmds.parentConstraint(camTrs, camToExport, mo=False)
+
+        # ... and bake it
+        cmds.bakeResults(camToExport,
+                         simulation=True,
+                         time=(minTime, maxTime),
+                         hi='none',
+                         sampleBy=1,
+                         oversamplingRate=1,
+                         disableImplicitControl=True,
+                         preserveOutsideKeys=True,
+                         sparseAnimCurveBake=False,
+                         removeBakedAttributeFromLayer=False,
+                         removeBakedAnimFromLayer=True,
+                         bakeOnOverrideLayer=False,
+                         shape=False)
+
+        cmds.delete(camConstraint)
+        currentFile = cmds.file(q=True, sn=True)
+
+        basePath = os.path.dirname(currentFile)
+        savePath = os.path.join(basePath, "{}.fbx".format(camTrs))
+
+        # export the camera as an fbx
+        cmds.select(camToExport)
+        cmds.file(savePath, exportSelected=True, type='FBX')
+        cmds.delete(camToExport)
+
